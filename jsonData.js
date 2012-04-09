9ffba742627516632b3679d75e572ada2bfb0cc4
Marc Fawzi <marc.fawzi@gmail.com>
2012-04-09 03:12:26 -0700

    updated to v0.2 ... many changes and fixes.. see index.html

---
+++ jsonData.js
@@ -0,0 +1,70 @@
+var jsonData = { "teams": [
+        	{
+        		"caption": "Team 1",
+        		"type": "poker",
+        		"members": [ 
+        		        
+        		        {
+	        		        "username": "John",	
+	        		        "id": "605768768",
+	        		        "data": "some data"	
+        		        },
+        		        {
+            		        "username": "George",	
+            		        "id": "87998798",
+            		        "data": "some data"	
+            		    },
+            		    {
+            		        "username": "Paul",	
+            		        "id": "79879898",
+            		        "otherData": "some data"	
+            		    }
+            		    
+        		        ],
+                		
+        	},
+        	
+        	{
+        		"caption": "Team 2",
+        		"type": "8 of spades",
+        		"members": [ 
+        	  
+        		        {
+        		        "username": "Joe",	
+        		        "id": "788768768",
+        		        "otherData": "some data"	
+        		        },
+        		        {
+            		        "username": "Hana",	
+            		        "id": "89090098",
+            		        "otherData": "some data"	
+            		    },
+            		    {
+            		        "username": "Joan",	
+            		        "id": "8779898",
+            		        "otherData": "some data"	
+            		    }
+            		    
+        		        ],
+        		
+        	},
+        	
+        	{
+        		"caption": "Team 3",
+        		"type": "double solitaire",
+        		"members": [ 
+        		
+        		        {
+        		        "username": "Dirk",	
+        		        "id": "87987987",
+        		        "otherData": "some data"	
+        		        },
+        		        {
+            		        "username": "Gentley",	
+            		        "id": "23443543",
+            		        "otherData": "some data"	
+            		    }
+        		        ],
+        	}
+        ]
+       };

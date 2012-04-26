6b2b2ee6f4625358111fdf5d43ddbe3b46610109
Marc Fawzi <marc.fawzi@gmail.com>
2012-04-25 22:33:39 -0700

    added one more data object in test case

--- jsonData.js
+++ jsonData.js
@@ -9,15 +9,15 @@ var jsonData = { "teams": [
 	        		        "id": "605768768",
 	        		        "data": "some data"	
         		        },
-        		        {
-            		        "username": "George",	
-            		        "id": "87998798",
-            		        "data": "some data"	
-            		    },
             		    {
             		        "username": "Paul",	
             		        "id": "79879898",
             		        "otherData": "some data"	
+            		    },
+            		    {
+            		        "username": "George",	
+            		        "id": "87998798",
+            		        "data": "some data"	
             		    }
             		    
         		        ],
@@ -62,6 +62,29 @@ var jsonData = { "teams": [
         		        {
             		        "username": "Gentley",	
             		        "id": "23443543",
+            		        "otherData": "some data"	
+            		    }
+        		        ],
+        	},
+        	
+        	{
+        		"caption": "Team 4",
+        		"type": "yet another card game",
+        		"members": [ 
+        		
+        		        {
+        		        "username": "Mike",	
+        		        "id": "8723234",
+        		        "otherData": "some data"	
+        		        },
+        		        {
+            		        "username": "Fay",	
+            		        "id": "4234421",
+            		        "otherData": "some data"	
+            		    },
+        		        {
+            		        "username": "Jared",	
+            		        "id": "89080900",
             		        "otherData": "some data"	
             		    }
         		        ],
